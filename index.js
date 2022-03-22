e930c89e6910d8375efaba7c828b9e99ddea19f4
Anusha Rudra <anusha.r@spaeanalytics.com>
2022-03-22 13:49:21 +0530

    cleardrawing

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const port = process.env.PORT || 4000;
 app.use(express.static(__dirname + '/public'));
 
 function onConnection(socket) {
-
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
   socket.on('image', (data) => socket.broadcast.emit('image', data));
-
+  socket.on('cleardrawing', (data) => socket.broadcast.emit('cleardrawing', data));
 }
+
 io.on('connection', onConnection);
+
 http.listen(port, () => console.log('listening on port ' + port));
